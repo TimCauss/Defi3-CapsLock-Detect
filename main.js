cea0b7d5a43f2be0144d9230ba9a4c75b108627b
TimCauss <t.caussignac@gmail.com>
2023-06-06 14:11:26 +0200

    test

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 const messageElem = document.getElementById("warning");
+const messageElem2 = document.getElementById("warning2");
 
 document.getElementById("input").addEventListener("keyup", (e) => {
-  e.getModifierState("CapsLock")
-    ? (messageElem.style.display = "block")
-    : (messageElem.style.display = "none");
+  if (e.getModifierState("CapsLock") && e.getModifierState("NumLock")) {
+    messageElem.innerHTML = "Caps Lock is activated... <br>";
+    messageElem.style.display = "block";
+    messageElem2.innerHTML = "Num Lock is activated... <br>";
+    messageElem2.style.display = "block";
+  } else if (e.getModifierState("CapsLock")) {
+    console.log(e);
+    messageElem.innerHTML = "Caps Lock is activated... <br>";
+    messageElem.style.display = "block";
+  } else if (e.getModifierState("NumLock")) {
+    console.log(e);
+    messageElem2.innerHTML = "Num Lock is activated... <br>";
+    messageElem2.style.display = "block";
+  } else {
+    messageElem.style.display = "none";
+    messageElem2.style.display = "none";
+  }
 });
