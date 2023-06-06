6519608a33b46faf7fbf5a7b7ba447515c7e1a21
TimCauss <t.caussignac@gmail.com>
2023-06-06 14:43:55 +0200

    test

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const messageElem = document.getElementById("warning");
 const messageElem2 = document.getElementById("warning2");
 
-window.addEventListener("keyup", (e) => {
-  e.getModifierState("CapsLock")
+window.addEventListener("keyup", (event) => {
+  event.getModifierState("CapsLock")
     ? (messageElem.style.display = "block")
     : (messageElem.style.display = "none");
-  e.getModifierState("NumLock")
+  event.getModifierState("NumLock")
     ? (messageElem2.style.display = "block")
     : (messageElem2.style.display = "none");
 });
